5a28eec78d82a2f9d31352b14ab187ec63bfccce
Lenvx <111296626+lenvxcodes@users.noreply.github.com>
2022-10-24 21:19:40 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 window.addEventListener("keydown", (key) => {
     if (key.keyCode === 18){
         let pagename = window.prompt("Prompt Page");
+        if (pagename == "list"){
+            alert("Blog,")
+            return
+        }
         window.location.href = 'https://lenvxcodes.netlify.app/' + pagename
     }
 });
