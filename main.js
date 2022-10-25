a2e5578a675bcfd109279326e15fe78eb6ba7ebe
Lenvx <111296626+lenvxcodes@users.noreply.github.com>
2022-10-25 19:16:19 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 window.addEventListener("keydown", (key) => {
     if (key.keyCode === 18){
-        let pagename = window.prompt("Prompt Page");
-        if (pagename == "list"){
-            alert("Blog,")
+        let promptpage = window.prompt("Prompt Page");
+        if (promptpage === "list"){
+            alert("Blog.")
             return
         }
-        window.location.href = 'https://lenvxcodes.netlify.app/' + pagename
+	if (promptpage === "dr blog"){
+        window.location.href = 'https://lenvxcodes.netlify.app/blog'
+	return
+	}
+	if (promptpage === "help"){
+	alert("dr - page redirector. list - page list")
+	return
+	}
+	if (promptpage === "new"){
+	alert("whats new?: added prompt")
+	return
+    	}		
+	else{
+	alert("Invalid Response")
+	return
+	}
     }
 });
